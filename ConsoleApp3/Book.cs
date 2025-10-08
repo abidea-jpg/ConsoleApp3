9363142fa9f7f076b293f36f9017ad7b28856701
abidea-jpg <abidea@code.edu.az>
2025-10-08 13:38:16 +0400

    taskli maskli

--- ConsoleApp3/Book.cs
+++ ConsoleApp3/Book.cs
@@ -2,6 +2,16 @@
 {
     internal class Book : Product
     {
+        public string AuthorName { get; set; }
+        public int PageCount { get; set; }
+
+        public Book(string name, double price, int count, string authorName, int pageCount)
+            : base(name, price, count)
+        {
+            AuthorName = authorName;
+            PageCount = pageCount;
+        }
+
         public override void Sell()
         {
             throw new NotImplementedException();
@@ -11,5 +21,6 @@
         {
             throw new NotImplementedException();
         }
+
     }
 }
